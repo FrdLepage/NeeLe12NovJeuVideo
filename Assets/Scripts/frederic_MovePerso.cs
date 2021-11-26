39bb56de214ebc3d83417ab91dd2920cdef2aa2a
Frédéric Lepage <56794713+FrdLepage@users.noreply.github.com>
2021-11-26 15:45:40 -0500

    Plane, Attenuateur, vie ajouter

--- Assets/Scripts/frederic_MovePerso.cs
+++ Assets/Scripts/frederic_MovePerso.cs
@@ -25,6 +25,7 @@ public class frederic_MovePerso : MonoBehaviour
     private Vector3 directionsMouvement= Vector3.zero;
     private bool peutAttaquer = true;
 
+    [SerializeField] private GameObject vie;
     Animator animator;
     CharacterController controller;
     // Start is called before the first frame update
@@ -33,9 +34,18 @@ public class frederic_MovePerso : MonoBehaviour
         animator=GetComponent<Animator>();
         controller=GetComponent<CharacterController>();
         _audio = GetComponent<AudioSource>();
-       
         
+        vie.GetComponent<SystemeDePoint>().SetMaxPoint(20);
+        vie.GetComponent<SystemeDePoint>().currentPoint=20;
         
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Perd une vie");
+       if (other.gameObject.tag=="Lave")
+       {
+           Debug.Log("Perd une vie sur la lave");
+       }
     }
 
     // Update is called once per frame
