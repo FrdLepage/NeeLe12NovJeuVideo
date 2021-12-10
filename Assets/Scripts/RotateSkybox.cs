21670525446f98362c98d33e2e700547f112c32c
raphaelBlais <78546236+raphaelBlais@users.noreply.github.com>
2021-12-10 13:38:08 -0500

    exposure change avec le temps, color +-

--- Assets/Scripts/RotateSkybox.cs
+++ Assets/Scripts/RotateSkybox.cs
@@ -5,16 +5,23 @@ using UnityEngine;
 public class RotateSkybox : MonoBehaviour
 {
 
-    public float speed;
+    [SerializeField] private Color _color;
+
+    public float _speedRotation;
+    public float _speedExposure;
+    // public float _speedTint;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * _speedRotation);
+        RenderSettings.skybox.SetFloat("_Exposure", Time.time * _speedExposure);
+        // RenderSettings.skybox.SetColor("_Tint", Color.red);
     }
 }
