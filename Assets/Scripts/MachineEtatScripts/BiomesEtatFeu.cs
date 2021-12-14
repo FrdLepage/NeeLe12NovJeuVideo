13292d385aed88fd49739c9ef8cb75a6526cc9e2
MarieFranceHetu <78545962+MarieFranceHetu@users.noreply.github.com>
2021-12-14 14:15:42 -0500

    debut des commentaires

--- Assets/Scripts/MachineEtatScripts/BiomesEtatFeu.cs
+++ Assets/Scripts/MachineEtatScripts/BiomesEtatFeu.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class BiomesEtatFeu : BiomesEtatsBase
 {
-
-  
   public override void InitEtat(BiomesEtatsManager biome)
   {
-      biome.StartCoroutine(Boule(biome));    
+      biome.StartCoroutine(Boule(biome)); //appel de la coroutine s'occupe de produire l'effet de feu
   }
-  // public override void UpdateEtat(BiomesEtatsManager biome)
-  // {
-    
-  // }
+  
+  /// <summary>
+  /// Permet de déclencher l'effet de feu sur les biomes
+  /// et donc d'instancier le système de particules.
+  /// </summary>
+  /// <param name="biome">biome d type BiomesEtatsManager</param>
   private IEnumerator Boule(BiomesEtatsManager biome){
-    var particules = Resources.Load("particules") as GameObject;
+    //Aller chercher le systeme de particules dans le dossier resources
+    GameObject particules = Resources.Load("particules") as GameObject;
+    //Instanciation du systeme de particules a la position du biome
     GameObject.Instantiate(particules, biome.transform.position, Quaternion.identity);
+    //l'etat du biome est changer vers l'etat activable
     biome.ChangerEtat(biome.activable);
     yield return null;
   }
